3855cbefaedd4bcae3d4d26e5bc99cdad9cbfed3
Sina Rueeger <sina.rueeger@gmail.com>
2018-01-08 19:22:07 +0100

    adding R file that starts everything

---
+++ start_blogdown.R
@@ -0,0 +1,10 @@
+
+## installation
+## https://bookdown.org/yihui/blogdown/installation.html
+library(blogdown)
+
+## https://bookdown.org/yihui/blogdown/a-quick-example.html
+blogdown::new_site(theme = "gcushen/hugo-academic")
+
+
+blogdown::hugo_build()
