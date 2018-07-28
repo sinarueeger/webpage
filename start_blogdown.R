5f2ef5c3f760d673a1d0e6c5b8333da3dcd2c361
Sina Rueeger <sina.rueeger@gmail.com>
2018-07-28 11:44:58 +0200

    adding blogpost locuszoomplot

--- start_blogdown.R
+++ start_blogdown.R
@@ -19,7 +19,7 @@ blogdown::hugo_build()
 ## 3) commit changes to github >> do in terminal
 path <- getwd()
 
-comment <- "'adding satrday cardiff data locke link '"
+comment <- "'adding blogpost locuszoomplot '"
 
 ## b) commit changes of webpage folder
 ## add files system("git add ...")
