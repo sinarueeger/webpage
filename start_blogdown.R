3d40eb182fc3d3f6cb81a493941dd3754b2079ed
Sina Rueeger <sina.rueeger@gmail.com>
2018-07-30 23:32:20 +0200

    spell checker

--- start_blogdown.R
+++ start_blogdown.R
@@ -19,7 +19,7 @@ blogdown::hugo_build()
 ## 3) commit changes to github >> do in terminal
 path <- getwd()
 
-comment <- "'changed link of resources tab'"
+comment <- "'spell checker'"
 
 ## b) commit changes of webpage folder
 ## add files system("git add ...")
