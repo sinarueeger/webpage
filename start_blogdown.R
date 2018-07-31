edf0e4b98cbba544f809416826d124d5319f112f
Sina Rueeger <sina.rueeger@gmail.com>
2018-07-31 10:35:02 +0200

    changing gplus to googlescholar icon

--- start_blogdown.R
+++ start_blogdown.R
@@ -19,7 +19,7 @@ blogdown::hugo_build()
 ## 3) commit changes to github >> do in terminal
 path <- getwd()
 
-comment <- "'fa items'"
+comment <- "'changing gplus to googlescholar icon'"
 
 ## b) commit changes of webpage folder
 ## add files system("git add ...")
