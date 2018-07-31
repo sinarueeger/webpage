1654568079e7ff2f41c62b6ed81cccfa21d04171
Sina Rueeger <sina.rueeger@gmail.com>
2018-07-31 11:19:51 +0200

    changing fonts (again?! :-)

--- start_blogdown.R
+++ start_blogdown.R
@@ -19,7 +19,7 @@ blogdown::hugo_build()
 ## 3) commit changes to github >> do in terminal
 path <- getwd()
 
-comment <- "'changed all fa_ icons to ii_'"
+comment <- "'changing fonts (again?! :-)'"
 
 ## b) commit changes of webpage folder
 ## add files system("git add ...")
