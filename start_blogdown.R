7e35fda1e2929c31dafb4c877eee8ba0f56746c6
Sina Rueeger <sina.rueeger@gmail.com>
2018-07-31 11:30:29 +0200

    fix typo podcast

--- start_blogdown.R
+++ start_blogdown.R
@@ -19,7 +19,7 @@ blogdown::hugo_build()
 ## 3) commit changes to github >> do in terminal
 path <- getwd()
 
-comment <- "'changing fonts (again?! :-)'"
+comment <- "'fix typo podcast'"
 
 ## b) commit changes of webpage folder
 ## add files system("git add ...")
