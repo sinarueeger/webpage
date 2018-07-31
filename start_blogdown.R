671ab97cbc239a7192bb37e8d3a5bd7bb2b67cc5
Sina Rueeger <sina.rueeger@gmail.com>
2018-07-31 11:35:39 +0200

    testing RSS

--- start_blogdown.R
+++ start_blogdown.R
@@ -19,7 +19,7 @@ blogdown::hugo_build()
 ## 3) commit changes to github >> do in terminal
 path <- getwd()
 
-comment <- "'fix typo podcast'"
+comment <- "'testing RSS'"
 
 ## b) commit changes of webpage folder
 ## add files system("git add ...")
