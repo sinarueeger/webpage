d59225562de3c1a40c2764df7d1cb09358570d8d
Sina Rueeger <sina.rueeger@gmail.com>
2018-07-31 10:32:26 +0200

    fa items

--- start_blogdown.R
+++ start_blogdown.R
@@ -19,7 +19,7 @@ blogdown::hugo_build()
 ## 3) commit changes to github >> do in terminal
 path <- getwd()
 
-comment <- "'itemize toc'"
+comment <- "'fa items'"
 
 ## b) commit changes of webpage folder
 ## add files system("git add ...")
