f4d612c6650a9abd771b633b922b765b16e36435
Sina Rueeger <sina.rueeger@gmail.com>
2018-08-02 11:50:24 +0200

    adding comments liza

--- start_blogdown.R
+++ start_blogdown.R
@@ -19,7 +19,7 @@ blogdown::hugo_build()
 ## 3) commit changes to github >> do in terminal
 path <- getwd()
 
-comment <- "'last read through + change color palette'"
+comment <- "'adding comments liza'"
 
 ## b) commit changes of webpage folder
 ## add files system("git add ...")
