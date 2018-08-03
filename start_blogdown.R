db2e6d355d12e8501bbf9b045db43c5eba54cd65
Sina Rueeger <sina.rueeger@gmail.com>
2018-08-03 15:10:16 +0200

    Adding arg GRCh = 37 (tip flavia)

--- start_blogdown.R
+++ start_blogdown.R
@@ -19,7 +19,7 @@ blogdown::hugo_build()
 ## 3) commit changes to github >> do in terminal
 path <- getwd()
 
-comment <- "'adding packages (feedback flavia)'"
+comment <- "'adapting p2 paste > paste0 (feedback flavia)'"
 
 ## b) commit changes of webpage folder
 ## add files system("git add ...")
