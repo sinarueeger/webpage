461bb597fdf7f9797577e374e069ac8e076da7c0
Sina Rueeger <sina.rueeger@gmail.com>
2018-08-03 16:08:05 +0200

    fixing

--- start_blogdown.R
+++ start_blogdown.R
@@ -19,7 +19,7 @@ blogdown::hugo_build()
 ## 3) commit changes to github >> do in terminal
 path <- getwd()
 
-comment <- "'trying to figure out why icon is not working'"
+comment <- "'fixing'"
 
 ## b) commit changes of webpage folder
 ## add files system("git add ...")
