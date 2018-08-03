0a610682e80a5de48eceee0bdc590657e37158a9
Sina Rueeger <sina.rueeger@gmail.com>
2018-08-03 15:52:32 +0200

    trying to figure out why icon is not working

--- start_blogdown.R
+++ start_blogdown.R
@@ -19,7 +19,7 @@ blogdown::hugo_build()
 ## 3) commit changes to github >> do in terminal
 path <- getwd()
 
-comment <- "'changed icon top'"
+comment <- "'trying to figure out why icon is not working'"
 
 ## b) commit changes of webpage folder
 ## add files system("git add ...")
