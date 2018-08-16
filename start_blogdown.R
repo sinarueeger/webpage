c8fd1c8ab6e56a1543a17faa28824907977d202c
Sina Rueeger <sina.rueeger@gmail.com>
2018-08-16 18:47:47 +0200

    changed resource link

--- start_blogdown.R
+++ start_blogdown.R
@@ -19,7 +19,7 @@ blogdown::hugo_build()
 ## 3) commit changes to github >> do in terminal
 path <- getwd()
  
-comment <- "'added html widget'"
+comment <- "'changed resource link'"
 
 ## b) commit changes of webpage folder
 ## add files system("git add ...")
