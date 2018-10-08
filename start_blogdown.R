43fabc65b128ccc9a0cca56f30213eebe0100f21
Sina Rueeger <sina.rueeger@gmail.com>
2018-10-08 23:09:33 +0200

    adding again cardiff post

--- start_blogdown.R
+++ start_blogdown.R
@@ -19,7 +19,7 @@ blogdown::hugo_build()
 ## 3) commit changes to github >> do in terminal
 path <- getwd()
  
-comment <- "'added RUG slides'"
+comment <- "'try and fix old posts'"
 
 ## b) commit changes of webpage folder
 ## add files system("git add ...")
