e1b2d0228a7e4c918639ddc97cc2f9d079c9967d
Sina Rueeger <sina.rueeger@gmail.com>
2018-10-13 14:00:15 +0200

    adding new blogpost

--- start_blogdown.R
+++ start_blogdown.R
@@ -19,7 +19,7 @@ blogdown::hugo_build()
 ## 3) commit changes to github >> do in terminal
 path <- getwd()
  
-comment <- "'try and fix old posts'"
+comment <- "'adding new blogpost'"
 
 ## b) commit changes of webpage folder
 ## add files system("git add ...")
