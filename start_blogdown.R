26e91e969a7686f3e4e6c11e98a28faf2b483d11
Sina Rueeger <sina.rueeger@gmail.com>
2018-10-31 19:49:13 +0100

    adding new resume

--- start_blogdown.R
+++ start_blogdown.R
@@ -19,7 +19,7 @@ blogdown::hugo_build()
 ## 3) commit changes to github >> do in terminal
 path <- getwd()
  
-comment <- "'changed img tag to video tag'"
+comment <- "'added phd thesis'"
 
 ## b) commit changes of webpage folder
 ## add files system("git add ...")
