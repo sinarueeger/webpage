ab141a527430704f497a4fb5351d03388a1551e4
Sina Rueeger <sina.rueeger@gmail.com>
2019-05-30 20:22:53 +0200

    removed two drafts

--- start_blogdown.R
+++ start_blogdown.R
@@ -19,7 +19,7 @@ blogdown::hugo_build()
 ## 3) commit changes to github >> do in terminal
 path <- getwd()
  
-comment <- "'added new website design (hugo academic)'"
+comment <- "'removed two drafts'"
 
 ## b) commit changes of webpage folder
 ## add files system("git add ...")
