88a007bc15ffa1561c87c06b873dfe7ef192982e
Sina Rueeger <sina.rueeger@gmail.com>
2019-06-07 17:39:16 +0200

    adding links

--- start_blogdown.R
+++ start_blogdown.R
@@ -19,7 +19,7 @@ blogdown::hugo_build()
 ## 3) commit changes to github >> do in terminal
 path <- getwd()
  
-comment <- "'adding poster blogpost'"
+comment <- "'adding links'"
 
 ## b) commit changes of webpage folder
 ## add files system("git add ...")
