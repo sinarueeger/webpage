146c998d2f768a9c8c62df688dc58365606ad829
Sina Rueeger <sina.rueeger@gmail.com>
2019-07-18 17:40:52 +0200

    publish useR draft

--- start_blogdown.R
+++ start_blogdown.R
@@ -19,7 +19,7 @@ blogdown::hugo_build()
 ## 3) commit changes to github >> do in terminal
 path <- getwd()
  
-comment <- "'adding useR draft'"
+comment <- "'publish useR draft'"
 
 ## b) commit changes of webpage folder
 ## add files system("git add ...")
