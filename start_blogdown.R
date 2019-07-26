d0917933de1b557def2e4e0659841c0610311104
Sina Rueeger <sina.rueeger@gmail.com>
2019-07-26 09:14:04 +0200

    Adding finemapping post

--- start_blogdown.R
+++ start_blogdown.R
@@ -19,7 +19,7 @@ blogdown::hugo_build()
 ## 3) commit changes to github >> do in terminal
 path <- getwd()
  
-comment <- "'Adding finemapping post draft'"
+comment <- "'Adding finemapping post'"
 
 ## b) commit changes of webpage folder
 ## add files system("git add ...")
