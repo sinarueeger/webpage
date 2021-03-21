73e74ba32ea44a4b402f886275957a5087a97f8d
Sina Rueger <sina.rueeger@gmail.com>
2021-03-21 13:57:46 +0100

    publish cbt draft

--- start_blogdown.R
+++ start_blogdown.R
@@ -19,7 +19,7 @@ blogdown::hugo_build()
 ## 3) commit changes to github >> do in terminal
 path <- getwd()
  
-comment <- "'publish ropensci post'"
+comment <- "'publish cbt draft'"
 
 ## b) commit changes of webpage folder
 ## add files system("git add ...")
